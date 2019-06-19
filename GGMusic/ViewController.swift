8232e4ac53db84672651624d7f60b3a9e396c317
Sarah Estrellado <sarah.estrellado@icloud.com>
2019-06-19 10:02:35 -0500

    initial commit

---
+++ GGMusic/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GGMusic
+//
+//  Created by Sarah Rebecca Estrellado on 6/19/19.
+//  Copyright © 2019 Sarah Rebecca Estrellado. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
