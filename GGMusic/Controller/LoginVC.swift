63fb9b87c12bc1c04fa8c156d2d8c3df332b5bf8
Sarah Estrellado <sarah.estrellado@icloud.com>
2019-07-09 10:49:45 -0500

    Updated LoginVC

--- GGMusic/Controller/LoginVC.swift
+++ GGMusic/Controller/LoginVC.swift
@@ -12,32 +12,40 @@ import CoreData
 
 class LoginVC: UIViewController {
     
-    @IBOutlet weak var emailTextField: UITextField!
-    @IBOutlet weak var passwordTextField: UITextField!
-    @IBOutlet weak var submitButton: UIButton!
-    @IBOutlet weak var signUpLabel: UILabel!
-    @IBOutlet weak var signUpButton: UIButton!
+    @IBOutlet weak var loginWithSpotifyButton: UIButton!
+    @IBOutlet weak var installSpotifyButton: UIButton!
+    @IBOutlet weak var installSpotifyLabel: UILabel!
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     
     override func viewDidLoad() {
+       // loginWithSpotifyButton.isHidden = true
+        installSpotifyButton.isHidden = true
         let client = SpotifyClient.sharedInstance()
         if (client.isSpotifyAppInstalled()) {
             if (client.isLoggedIn()) {
                 startPlaylistCollectionVC()
             } else {
                 // TODO Show "Login with Spotify" button.
+                loginWithSpotifyButton.isHidden = false
             }
         } else {
+            installSpotifyButton.isHidden = false
             // TODO Show "Install Spotify" button.
             // When clicked should show the Spotify app in the App Store.
+            
         }
     }
     
-    @IBAction func submit(_ sender: Any) {
+    @IBAction func loginWithSpotify(_ sender: Any) {
+        activityIndicator.startAnimating()
         loginWithSpotify()
     }
     
-    @IBAction func signUp(_ sender: Any) {
+    @IBAction func installSpotifyButton(_ sender: Any) {
+        if let url = URL(string: "itms-apps://apps.apple.com/us/app/spotify-music-and-podcasts/id324684580"),
+            UIApplication.shared.canOpenURL(url){
+            UIApplication.shared.openURL(url)
+        }
     }
     
     private func loginWithSpotify() {
@@ -46,6 +54,7 @@ class LoginVC: UIViewController {
                 self.startPlaylistCollectionVC()
             },
             failed: {
+                self.activityIndicator.stopAnimating()
                 self.displayError("Login failed. Please try again.")
             }
         )
